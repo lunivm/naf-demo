d516ed0a5bb27394397e506a4c31dad59ca974db
Mykola Luniv <lunivm@yahoo.com>
2025-01-23 00:59:38 +0200

    improve table styling

--- apps/naf-demo/src/app/dashboard/requests-table/requests-table.component.ts
+++ apps/naf-demo/src/app/dashboard/requests-table/requests-table.component.ts
@@ -2,17 +2,19 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { RequestResponse, RequestsTableService } from './requests-table.service';
+import { Card } from 'primeng/card';
+import { Tab, Tabs, TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'naf-requests-table',
-  imports: [ CommonModule, TableModule ],
+  imports: [CommonModule, TableModule, Card, TabsModule],
   templateUrl: './requests-table.component.html',
   styleUrl: './requests-table.component.scss',
 })
 export class RequestsTableComponent implements OnInit {
   requests = signal<RequestResponse[]>([]);
 
-  private readonly requestsTableService = inject(RequestsTableService)
+  private readonly requestsTableService = inject(RequestsTableService);
 
   async ngOnInit() {
     this.requests.set(await this.requestsTableService.getData());
