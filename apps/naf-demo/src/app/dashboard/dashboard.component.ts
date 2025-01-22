be135df1e8ae8d124b09cac4b26c0ac59a730731
Mykola Luniv <lunivm@yahoo.com>
2025-01-22 12:41:37 +0200

    add dashboard layout; use css vars for indents

--- apps/naf-demo/src/app/dashboard/dashboard.component.ts
+++ apps/naf-demo/src/app/dashboard/dashboard.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from '../shared/components/toolbar/toolbar.component';
 import { Button } from 'primeng/button';
+import { Card } from 'primeng/card';
 
 @Component({
   selector: 'naf-dashboard',
-  imports: [CommonModule, ToolbarComponent, Button],
+  imports: [CommonModule, ToolbarComponent, Button, Card],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
