c37eb9d0d93468118cd61198eb9a7248107b8a5e
Mykola Luniv <lunivm@yahoo.com>
2025-01-22 14:23:24 +0200

    add charts proto

--- apps/naf-demo/src/app/app.routes.ts
+++ apps/naf-demo/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const appRoutes: Route[] = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
--- apps/naf-demo/src/app/shared/components/chart-card/chart-card.component.ts
+++ apps/naf-demo/src/app/shared/components/chart-card/chart-card.component.ts
@@ -1,13 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Card } from 'primeng/card';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @Component({
   selector: 'naf-chart-card',
-  imports: [CommonModule, Card],
+  imports: [ CommonModule, Card, NgApexchartsModule ],
   templateUrl: './chart-card.component.html',
   styleUrl: './chart-card.component.scss',
 })
 export class ChartCardComponent {
   title = input('');
+  chart = input<any>({ type: 'area' });
+  series = input<any>([{
+    data: [23, 34, 12, 54, 32, 43]
+}])
 }
