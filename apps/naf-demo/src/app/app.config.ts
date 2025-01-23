3ac9558096916f9d0cd00a9157cee371cc3345a5
Mykola Luniv <lunivm@yahoo.com>
2025-01-23 13:24:10 +0200

    improve sidebar styling; change color scheme to appropriate; add dark scheme tweak

--- apps/naf-demo/src/app/app.config.ts
+++ apps/naf-demo/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import Aura from '@primeng/themes/aura';
+import Material from '@primeng/themes/material';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
@@ -14,8 +14,8 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
-      }
+        preset: Material,
+      },
     }),
   ],
 };
