e53a3837d7f909c5f9a9ba9dfc0b9e5505f38d1c
Mykola Luniv <lunivm@yahoo.com>
2025-01-23 12:23:22 +0200

    improve toolbar styling

--- apps/naf-demo/src/app/dashboard/categories-chart/categories-chart.component.ts
+++ apps/naf-demo/src/app/dashboard/categories-chart/categories-chart.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApexChart, ChartComponent } from 'ng-apexcharts';
 import { CategoriesChartService } from './categories-chart.service';
+import { ApexGrid } from 'ng-apexcharts/lib/model/apex-types';
 
 @Component({
   selector: 'naf-categories-chart',
@@ -15,7 +16,12 @@ export class CategoriesChartComponent implements OnInit {
       show: false
     },
     type: 'donut',
-  }
+  };
+  readonly gridOptions: ApexGrid = {
+    padding: {
+      left: 50
+    }
+  };
 
   series = signal<number[]>([]);
   labels = signal<string[]>([]);
--- apps/naf-demo/src/app/shared/components/toolbar/toolbar.component.ts
+++ apps/naf-demo/src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,15 +1,14 @@
 import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Button } from 'primeng/button';
 import { MatIcon } from '@angular/material/icon';
 import { Toolbar } from 'primeng/toolbar';
 
 @Component({
   selector: 'naf-toolbar',
-  imports: [CommonModule, Button, MatIcon, Toolbar],
+  imports: [CommonModule, MatIcon, Toolbar],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
-  title = input('')
+  title = input('');
 }
